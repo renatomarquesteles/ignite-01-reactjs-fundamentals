5a761f49c262a10aeb16e6c59e7b950a6f000785
Renato <renatomarquesteles@gmail.com>
2022-06-25 20:13:18 -0300

    adds component props

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post author="Renato Marques" content="Good morning!" />
+      <Post author="Diego Fernandes" content="Good night!" />
     </div>
   );
 }
