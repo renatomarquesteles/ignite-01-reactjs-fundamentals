fb5f11197dcf2351cdcc8d9857d9afba2a6ae539
Renato <renatomarquesteles@gmail.com>
2022-06-25 20:30:24 -0300

    adds Header component with css modules to style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="Renato Marques" content="Good morning!" />
       <Post author="Diego Fernandes" content="Good night!" />
     </div>
